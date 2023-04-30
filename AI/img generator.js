b4321c2ad6a005c2ebbeee226daa1dbaef616ce8
GOOGLY_RTX <95157270+googly1030@users.noreply.github.com>
2023-04-30 20:30:09 +0530

    Add files via upload

---
+++ AI/img generator.js
@@ -0,0 +1,39 @@
+const API_KEY = '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';
+const submiticon = document.querySelector('#submit-icon');
+const inputElement= document.querySelector('input');
+const imageSection = document.querySelector('.image-section');
+
+const getImages = async () => { 
+    const options = {
+        method: 'POST',
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            'prompt': inputElement.value,
+            'n':2,
+            'size':"1024x1024"
+        })
+    }
+    try 
+    {
+       const response = await fetch('https://api.openai.com/v1/images/generations', options)
+         const data = await response.json()
+       
+            data?.data.forEach((imageObject) => 
+            {
+                const ImageContainer = document.createElement('div')
+                ImageContainer.classList.add('image-container')
+                const imageElement = document.createElement('img')
+                imageElement.setAttribute('src', imageObject.url)
+                ImageContainer.append(imageElement)
+                imageSection.append(ImageContainer)
+            })
+    }
+    catch (error)
+    {
+        console.log(error)
+    }
+}
+submiticon.addEventListener('click', getImages)
